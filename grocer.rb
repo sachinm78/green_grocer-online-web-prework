5e099eaf5afbb08344d551a56a21cba9c048aebb
Sachin Mahajan <sachinm78@gmail.com>
2019-03-06 17:31:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -49,5 +49,12 @@ end
 
 
 def checkout(items, coupons)
+  cart = consolidate_cart(items)
+  cart1 = apply_coupons(cart, coupons)
+  cart2 = apply_clearance(cart1)
   
+  total = 0
+  
+  cart2.each do |name, price_hash|
+    total += price_hash[:price] * price_hash[:count]
 end
