db203367ee0cf4ff4bd2ddfcf8c1a870ac3d28b8
Sachin Mahajan <sachinm78@gmail.com>
2019-03-06 17:32:39 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -52,9 +52,9 @@ def checkout(items, coupons)
   cart = consolidate_cart(items)
   cart1 = apply_coupons(cart, coupons)
   cart2 = apply_clearance(cart1)
-  
   total = 0
-  
   cart2.each do |name, price_hash|
     total += price_hash[:price] * price_hash[:count]
+  end
+  total > 100 ? total * 0.9 : total
 end
